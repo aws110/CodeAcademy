71978bce70fbe8066196a7d027a6bd5f9d396d25
Adam Simonar <adamsimonar@Adams-MacBook-Pro.local>
2019-03-08 13:33:28 -0600

    completed back end handler for get and put

--- Back end/project-2-beat-mix/presetHandler.js
+++ Back end/project-2-beat-mix/presetHandler.js
@@ -2,7 +2,27 @@
 const presets = require('./presets');
 
 // Complete this function:
-const presetHandler = () => {};
+const presetHandler = (request_type, index, newPresetArray) => {
+  debugger;
+  if (index < 0 || index > 15){
+    return [404];
+  }else if(!(request_type === 'GET' || request_type === 'PUT')){
+    return [400];
+  }
+  //index is OK so first element in final array is 200
+  let returnedArray =[200];
+  if (request_type === 'GET'){
+    returnedArray.push(presets[index]);
+    return returnedArray;
+  }else if (request_type === 'PUT'){
+    if(!newPresetArray){
+      return returnedArray;
+    }
+    presets[index] = newPresetArray;
+    returnedArray.push(presets[index]);
+    return returnedArray;
+  }
+};
 
 // Leave this line so that your presetHandler function can be used elsewhere:
 module.exports = presetHandler;
